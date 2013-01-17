4afa32a40b1a78bf4629294f5b692eff43320235
Tino Ehrich <ehrich@efides.com>
2013-01-17 17:21:48 +0100

    implemented request method to fetch transaction data based on merchant transaction id

--- src/Simplon/Payment/Skrill/SkrillProcess.php
+++ src/Simplon/Payment/Skrill/SkrillProcess.php
@@ -2,7 +2,38 @@
 
   namespace Simplon\Payment\Skrill;
 
-  class SkrillProcess
-  {
+  use Simplon\Payment\Skrill\Vo\CheckoutQueryResponseVo;
 
+  class SkrillProcess extends SkrillBase
+  {
+    /**
+     * @param $merchantEmailAccount
+     * @param $gatewayPassword
+     * @param $transactionId
+     * @return Vo\CheckoutQueryResponseVo
+     */
+    public function getCheckoutDetailsByMerchantTransactionId($merchantEmailAccount, $gatewayPassword, $transactionId)
+    {
+      // build query string
+      //      $postDataQuery = http_build_query($postData);
+      $parameter = 'action=status_trn&email=' . $merchantEmailAccount . '&password=' . md5($gatewayPassword) . '&trn_id=' . $transactionId;
+
+      // call paypal
+      $response = $this
+        ->_getCurlClass()
+        ->init($this->_getUrlQueryGateway() . '?' . $parameter)
+        ->setReturnTransfer(TRUE)
+        ->execute();
+
+      // remove first line
+      $response = preg_replace('/200\s*ok.*?\n/i', '', chop($response));
+
+      // parse response
+      parse_str($response, $data);
+
+      /** @var $checkoutResponseVo CheckoutQueryResponseVo */
+      $checkoutResponseVo = (new CheckoutQueryResponseVo())->setData($data);
+
+      return $checkoutResponseVo;
+    }
   }
