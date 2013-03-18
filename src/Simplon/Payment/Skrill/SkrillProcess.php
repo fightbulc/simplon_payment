805ab473b4b00c58ff94dc41f3bb9ab7769a33ee
Tino Ehrich <ehrich@efides.com>
2013-03-18 13:18:04 +0100

    handle error messages

--- src/Simplon/Payment/Skrill/SkrillProcess.php
+++ src/Simplon/Payment/Skrill/SkrillProcess.php
@@ -1,68 +1,82 @@
 <?php
 
-  namespace Simplon\Payment\Skrill;
+    namespace Simplon\Payment\Skrill;
 
-  use Simplon\Payment\Skrill\Vo\CheckoutQueryResponseVo;
+    use Simplon\Payment\Skrill\Vo\CheckoutQueryResponseVo;
 
-  class SkrillProcess extends SkrillBase
-  {
-    /**
-     * @param $merchantEmailAccount
-     * @param $gatewayPassword
-     * @param $transactionId
-     * @return Vo\CheckoutQueryResponseVo
-     */
-    public function getCheckoutDetailsByMerchantTransactionId($merchantEmailAccount, $gatewayPassword, $transactionId)
+    class SkrillProcess extends SkrillBase
     {
-      // build query string
-      // $postDataQuery = http_build_query($postData);
-      $parameter = 'action=status_trn&email=' . $merchantEmailAccount . '&password=' . md5($gatewayPassword) . '&trn_id=' . $transactionId;
+        /** @var array */
+        protected $_acceptableErrorCodes = [401, 402, 403, 404, 405];
 
-      // call paypal
-      $response = $this
-        ->_getCurlClass()
-        ->init($this->_getUrlQueryGateway() . '?' . $parameter)
-        ->setReturnTransfer(TRUE)
-        ->execute();
+        // ######################################
 
-      // parse response && throw Exception if failed
-      $data = $this->_parseResponse($response);
+        /**
+         * @param $merchantEmailAccount
+         * @param $gatewayPassword
+         * @param $transactionId
+         * @return Vo\CheckoutQueryResponseVo
+         */
+        public function getCheckoutDetailsByMerchantTransactionId($merchantEmailAccount, $gatewayPassword, $transactionId)
+        {
+            // build query string
+            // $postDataQuery = http_build_query($postData);
+            $parameter = 'action=status_trn&email=' . $merchantEmailAccount . '&password=' . md5($gatewayPassword) . '&trn_id=' . $transactionId;
 
-      /** @var $checkoutResponseVo CheckoutQueryResponseVo */
-      $checkoutResponseVo = (new CheckoutQueryResponseVo())->setData($data);
+            // call paypal
+            $response = $this
+                ->_getCurlClass()
+                ->init($this->_getUrlQueryGateway() . '?' . $parameter)
+                ->setReturnTransfer(TRUE)
+                ->execute();
 
-      return $checkoutResponseVo;
+            // parse response && throw Exception if failed
+            $data = $this->_parseResponse($response);
+
+            /** @var $checkoutResponseVo CheckoutQueryResponseVo */
+            $checkoutResponseVo = (new CheckoutQueryResponseVo())->setData($data);
+
+            return $checkoutResponseVo;
+        }
+
+        // ##########################################
+
+        /**
+         * @param $response
+         * @return mixed
+         * @throws \Exception
+         */
+        protected function _parseResponse($response)
+        {
+            // remove trailing NEW_LINE
+            $response = chop($response);
+
+            // if NOT code 200 --> filter error message
+            if(! preg_match('/200\s*ok.*?/', $response))
+            {
+                // clear response format
+                $response = preg_replace('/\s+/u', ' ', $response);
+
+                // get error code from response
+                preg_match('/^(\d{3})/', $response, $regexp);
+                $errorCode = $regexp[1];
+
+                // if unknown error code set to 500
+                if(! in_array($errorCode, $this->_acceptableErrorCodes))
+                {
+                    $errorCode = 500;
+                }
+
+                // throw exception with response and error code
+                throw new \Exception(__CLASS__ . ': failed response code: <' . $response . '>', $errorCode);
+            }
+
+            // remove response code
+            $response = preg_replace('/200\s*ok.*?\n/i', '', $response);
+
+            // parse response
+            parse_str($response, $data);
+
+            return $data;
+        }
     }
-
-    // ##########################################
-
-    /**
-     * @param $response
-     * @return mixed
-     * @throws \Exception
-     */
-    protected function _parseResponse($response)
-    {
-      // remove trailing NEW_LINE
-      $response = chop($response);
-
-      /**
-       * We need to filter out any failings. For instance:
-       * 401   Cannot login: remote ip (1.2.3.4) is not in list of allowed ips
-       */
-
-      // check if we got valid response
-      if(! preg_match('/200\s*ok.*?/', $response))
-      {
-        throw new \Exception(__CLASS__ . ': failed response: <' . $response . '>');
-      }
-
-      // remove response code
-      $response = preg_replace('/200\s*ok.*?\n/i', '', chop($response));
-
-      // parse response
-      parse_str($response, $data);
-
-      return $data;
-    }
-  }
